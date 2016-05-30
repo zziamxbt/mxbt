1808bd2e72f07fb5b10e224a11f21600443dc991
ljj <2969178131@qq.com>
2016-05-30 10:33:02 +0800

    change ljj

--- regist/src/main/java/com/qiyu/regist/LoginActivity.java
+++ regist/src/main/java/com/qiyu/regist/LoginActivity.java
@@ -43,8 +43,7 @@ import cn.smssdk.gui.RegisterPage;
 public class LoginActivity extends AppCompatActivity {
     /**
      * 用户的正常登录途径(ffffffffffffffffffff)
-     * wty
-急急急
+
      */
     private EditText mUname;//用户帐号
     private EditText mUpassword;//用户密码
