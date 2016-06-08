4e0932e9f31ee0ffc274e58146577b5b1f31d99c
wlf <571663324@qq.com>
2016-06-08 10:07:07 +0800

    wulongfei

--- app/src/main/java/com/example/acer/zzia_mxbt/activity/MainActivity.java
+++ app/src/main/java/com/example/acer/zzia_mxbt/activity/MainActivity.java
@@ -96,21 +96,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     //是否登录
-<<<<<<< HEAD
+
     private static  boolean isLogin = false;
-=======
-    private boolean isLogin = false;
->>>>>>> ee39f3392c57094014cb703193776a99a327c2c7
 
     public static User getUser() {
         return user;
     }
-<<<<<<< HEAD
+
     public static boolean isLogin() {
         return isLogin;
     }
-=======
->>>>>>> ee39f3392c57094014cb703193776a99a327c2c7
 
     private static User user;
     //双击退出标志位
@@ -125,8 +120,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
-       // JPushInterface.setDebugMode(true);
-        JPushInterface.init(this);
+        JPushInterface.setDebugMode(true);
         getLoginParam();
         initView();
         initData();
--- app/src/main/java/com/example/acer/zzia_mxbt/application/MyApplication.java
+++ app/src/main/java/com/example/acer/zzia_mxbt/application/MyApplication.java
@@ -25,50 +25,40 @@ public class MyApplication extends Application {
     //登录
     private String loginUrl = "http://10.201.1.115:8080/ZZIA_MXBT/loginCheckServlet";
     //显示专题封面
-<<<<<<< HEAD
-    private String showSubjectArticleUrl = "http://10.201.1.183:80/ZZIA_MXBT/showSubjectArticleServlet";
-
-    private String showSubjectUrl = "http://10.201.1.183:80/ZZIA_MXBT/showSubjectServlet";
-=======
 
 
     private String showSubjectUrl = "http://10.201.1.115:80/ZZIA_MXBT/showSubjectServlet";
     //显示专题对应文章
     private String showSubjectArticleUrl = "http://10.201.1.115:80/ZZIA_MXBT/showSubjectArticleServlet";
 
->>>>>>> ee39f3392c57094014cb703193776a99a327c2c7
+
 
     //排行榜
     private String author_url = "http://10.201.1.115:8080/ZZIA_MXBT/user_servlet";
     private String novel_url = "http://10.201.1.115:8080/ZZIA_MXBT/novel_servlet";
     //投票
-<<<<<<< HEAD
-    private String vote_url = "http://10.201.1.183:8080/ZZIA_MXBT/vote_servlet";
+
+    private String vote_url = "http://10.201.1.115:8080/ZZIA_MXBT/vote_servlet";
 
 
     //个人中心
     //我的故事
-    private static  String mystory_url="http://10.201.1.183:8080/ZZIA_MXBT/mystory";
+    private static  String mystory_url="http://10.201.1.115:8080/ZZIA_MXBT/mystory";
     //我的收藏
-    private static  String mycollect_url="http://10.201.1.183:8080/ZZIA_MXBT/mycollect";
+    private static  String mycollect_url="http://10.201.1.115:8080/ZZIA_MXBT/mycollect";
 
 
     //我的推荐
-    private static  String myrecommand_url="http://10.201.1.183:8080/ZZIA_MXBT/myrecommand";
+    private static  String myrecommand_url="http://10.201.1.115:8080/ZZIA_MXBT/myrecommand";
 
 
     public static String getMycollect_url() {
         return mycollect_url;
     }
 
-=======
-    private String vote_url = "http://10.201.1.115:8080/ZZIA_MXBT/vote_servlet";
-
-
-
     //个人中心
     private static  String center_url="http://10.201.1.115:8080/ZZIA_MXBT/index_servlet";
->>>>>>> ee39f3392c57094014cb703193776a99a327c2c7
+
 
     @Override
     public void onCreate() {
@@ -115,7 +105,6 @@ public class MyApplication extends Application {
     public String getShowSubjectArticleUrl() {
         return showSubjectArticleUrl;
     }
-<<<<<<< HEAD
     public static String getMystory_url() {
         return mystory_url;
     }
@@ -124,12 +113,12 @@ public class MyApplication extends Application {
         return myrecommand_url;
     }
 
-=======
+
 
     public static String getCenter_url() {
         return center_url;
     }
 
 
->>>>>>> ee39f3392c57094014cb703193776a99a327c2c7
+
 }
